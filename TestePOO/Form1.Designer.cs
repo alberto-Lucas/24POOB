b4acf660f44613fcd6e0e2ab453ae6e2e86ede99
alberto-Lucas <100029506+alberto-Lucas@users.noreply.github.com>
2024-08-22 22:26:06 -0300

    TestePOO aula dia 22/08/2024
    TestePOO aula dia 22/08/2024

---
+++ TestePOO/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace TestePOO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpDtNascimento = new System.Windows.Forms.DateTimePicker();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtVisCPF = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtVisNome = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtVisDtNascimento = new System.Windows.Forms.TextBox();
+            this.btnCarregar = new System.Windows.Forms.Button();
+            this.lblCPFNome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(12, 25);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(12, 78);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(100, 20);
+            this.txtCPF.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CPF:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Data Nascimento";
+            // 
+            // dtpDtNascimento
+            // 
+            this.dtpDtNascimento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDtNascimento.Location = new System.Drawing.Point(12, 132);
+            this.dtpDtNascimento.Name = "dtpDtNascimento";
+            this.dtpDtNascimento.Size = new System.Drawing.Size(100, 20);
+            this.dtpDtNascimento.TabIndex = 5;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(15, 158);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 6;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(163, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Data Nascimento";
+            // 
+            // txtVisCPF
+            // 
+            this.txtVisCPF.Location = new System.Drawing.Point(163, 78);
+            this.txtVisCPF.Name = "txtVisCPF";
+            this.txtVisCPF.Size = new System.Drawing.Size(100, 20);
+            this.txtVisCPF.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(163, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "CPF:";
+            // 
+            // txtVisNome
+            // 
+            this.txtVisNome.Location = new System.Drawing.Point(163, 25);
+            this.txtVisNome.Name = "txtVisNome";
+            this.txtVisNome.Size = new System.Drawing.Size(100, 20);
+            this.txtVisNome.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(163, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Nome:";
+            // 
+            // txtVisDtNascimento
+            // 
+            this.txtVisDtNascimento.Location = new System.Drawing.Point(163, 132);
+            this.txtVisDtNascimento.Name = "txtVisDtNascimento";
+            this.txtVisDtNascimento.Size = new System.Drawing.Size(100, 20);
+            this.txtVisDtNascimento.TabIndex = 12;
+            // 
+            // btnCarregar
+            // 
+            this.btnCarregar.Location = new System.Drawing.Point(177, 158);
+            this.btnCarregar.Name = "btnCarregar";
+            this.btnCarregar.Size = new System.Drawing.Size(75, 23);
+            this.btnCarregar.TabIndex = 13;
+            this.btnCarregar.Text = "Carregar";
+            this.btnCarregar.UseVisualStyleBackColor = true;
+            this.btnCarregar.Click += new System.EventHandler(this.btnCarregar_Click);
+            // 
+            // lblCPFNome
+            // 
+            this.lblCPFNome.AutoSize = true;
+            this.lblCPFNome.Location = new System.Drawing.Point(15, 211);
+            this.lblCPFNome.Name = "lblCPFNome";
+            this.lblCPFNome.Size = new System.Drawing.Size(35, 13);
+            this.lblCPFNome.TabIndex = 14;
+            this.lblCPFNome.Text = "label7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 255);
+            this.Controls.Add(this.lblCPFNome);
+            this.Controls.Add(this.btnCarregar);
+            this.Controls.Add(this.txtVisDtNascimento);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtVisCPF);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtVisNome);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.dtpDtNascimento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpDtNascimento;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtVisCPF;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtVisNome;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtVisDtNascimento;
+        private System.Windows.Forms.Button btnCarregar;
+        private System.Windows.Forms.Label lblCPFNome;
+    }
+}
+
---
+++ TestePOO/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestePOO
+{
+    public partial class Form1 : Form
+    {
+        //Criamos a classe Pessoa
+        public class Pessoa
+        {
+            //Definos os atributos
+            //O atributo é divido em 4 partes
+            //Nivel de Acesso: Public ou Private
+            //Tipo de Dado: Int, String, ...
+            //O Nome: Nome, CPF, Altura...
+            //Modificadore: Get/Set
+            public string Nome { get; set; }
+            //O VS possui o atalho PROP
+            public string CPF { get; set; }
+            public DateTime DtNascimento { get; set; }
+
+            //vamos criar um construtor, para definir
+            //o valor padrão de cada atributo
+            public Pessoa()
+            {
+                Nome = "Desconhecido";
+                CPF = "00000000000";
+                DtNascimento = DateTime.Now;
+            }
+
+            public string CPFNome 
+            {
+                get
+                {
+                    return CPF + " - " +
+                            Nome.ToUpper();
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Pessoa pessoaBKP = new Pessoa();
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Pessoa instancia = new Pessoa();
+            instancia.Nome = txtNome.Text;
+            instancia.CPF = txtCPF.Text;
+            instancia.DtNascimento = 
+                dtpDtNascimento.Value;
+
+            pessoaBKP = instancia;
+
+        }
+
+        private void btnCarregar_Click(object sender, EventArgs e)
+        {
+            txtVisNome.Text = pessoaBKP.Nome;
+            txtVisCPF.Text = pessoaBKP.CPF;
+            txtVisDtNascimento.Text =
+                pessoaBKP.DtNascimento.
+                ToShortDateString();
+
+            lblCPFNome.Text = pessoaBKP.CPFNome;
+        }
+    }
+}
